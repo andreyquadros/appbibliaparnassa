a75fbd912c8ca1d06093f9ea2d3eceda491285f8
Andrey Quadros <andreyquadros@users.noreply.github.com>
2026-04-23 13:32:04 -0400

    build: rename Android package to bibliaparnassa

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 }
 
 android {
-    namespace = "com.palavraviva.palavra_viva"
+    namespace = "br.com.andreyquadros.bibliaparnassa"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -39,7 +39,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.palavraviva.palavra_viva"
+        applicationId = "br.com.andreyquadros.bibliaparnassa"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/br/com/andreyquadros/bibliaparnassa/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.andreyquadros.bibliaparnassa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
